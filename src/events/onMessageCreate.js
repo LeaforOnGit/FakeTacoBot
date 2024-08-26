596a85f9e3b3d2e228cbf6090ed4b7ca6ba6dc28
Rafael Camelo <Rafael77001@hotmail.com>
2024-08-26 17:08:00 +0100

    changes before publishing branch

--- src/events/onMessageCreate.js
+++ src/events/onMessageCreate.js
@@ -2,24 +2,20 @@ import { addTaco } from "../database/operations/addTaco.js";
 import { saveTacoMessage } from "../database/operations/saveTacoMessage.js";
 
 export const onMessageCreate = async (message) =>{
-    // console.log("\n");
-    // console.log(message);
 
+    //No tacos here sadge 
     if(!message.content.includes('🌮')){
-        //No tacos here sadge 
         console.log("NO TACOS HERE YOU DUMMY");
         return;
     }
 
+    //Currently this will only support 1 mention
     if(message.mentions.users.size != 1){
-        //Currently this will only support 1 mention
         console.log("ONE MENTION AT A TIME YOU DUMMY");
         return;
     }
 
     const userMentioned = message.mentions.users.keys().next().value;
-
-    // console.log(`Received msg: ${message.content} \n\tWith mention to user: ${userMentioned}\n\tFrom user: ${message.author.id}`);
 
     if(addTaco(userMentioned, message.author.id)){
         saveTacoMessage(message.content, userMentioned, message.author.id);
